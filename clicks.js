f0bb02317a84e4dea69e5ee721d8547ad98b727d
Pawel Rusin <rusin.paw@gmail.com>
2017-03-04 15:22:49 +0900

    remove dummy post content

--- clicks.js
+++ clicks.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
 $('#button').click(function() {
 	console.log("smiec");
-    $.post( "https://swiftscript.mybluemix.net/", "test", function(data) {
-    	$('#right').text("test");	
+    $.post( "https://swiftscript.mybluemix.net/", $('#button').text(), function(data) {
+    	$('#right').text(data);	
     });	
 });
 });
