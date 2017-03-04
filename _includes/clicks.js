c946d7fd48733041c11177b08a5549387a950519
Pawel Rusin <rusin.paw@gmail.com>
2017-03-04 16:03:21 +0900

    remove javascript

--- _includes/clicks.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-$('#button').on('click', function() {
-	console.log("smiec");
-    $.post( "https://swiftscript.mybluemix.net/", "test", function(data) {
-    	$('#right').text("test");	
-    });	
-});
-})(jQuery);
